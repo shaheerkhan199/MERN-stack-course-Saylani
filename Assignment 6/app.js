498f83214f2cae326ea40dd54372320a2f4c4b4c
M.Shaheer khan <m.shaheerkhan199@gmail.com>
2020-06-18 06:45:43 -0500

    Working on assignment 6

---
+++ Assignment 6/app.js
@@ -0,0 +1,55 @@
+// chapters 21-25
+// Task 1
+    // var firstName = prompt("Enter your first name");
+    // var lastName = prompt("Enter your last name");
+    // var fullName = firstName+" "+lastName;
+    // alert("Hello "+ fullName + " Good to see you.")
+// Task 2
+    // var favPhone = prompt("Enter your favourite Phone name and model");
+    // var lengthOfStr = favPhone.length;
+    // document.write("My favourite phone is : " + favPhone + "<br>");
+    // document.write("Length of string: "+ lengthOfStr);
+// Task 3
+    // var country = "Pakistani";
+    // var indexOfn = country.indexOf('n');
+    // document.write("string: "+ country + "<br>");
+    // document.write("index of \'n\' is: "+indexOfn);
+// Task 4
+    // var str = "Hello World";
+    // var indexOfl = str.lastIndexOf('l');
+    // document.write("String: "+ str + "<br>");
+    // document.write("Last index of \'l\' is: "+ indexOfl);
+
+// Task 5
+    // var country = "Pakistani";
+    // var char = country.charAt(3);
+    // document.write("String: "+ country + "<br>");
+    // document.write("Character at index 3 is: "+ char); 
+// Task 6
+    // var firstName = prompt("Enter your first name");
+    // var lastName = prompt("Enter your last name");
+    // var fullName = firstName.concat(" ",lastName);
+    // alert("Hello "+ fullName + " Good to see you.")
+// Task 7
+
+// Task 8
+
+// Task 9
+
+// Task 10
+
+// Task 11
+
+// Task 12
+
+// Task 13
+
+// Task 14
+
+// Task 15
+
+// Task 16
+
+// Task 17
+
+// Task 18
