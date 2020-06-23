12ff018eb35be1343c94649a81d3f244e1d75434
M.Shaheer khan <m.shaheerkhan199@gmail.com>
2020-06-23 19:24:40 +0500

    Some UI changes

--- Assignment 4/public/js/script.js
+++ Assignment 4/public/js/script.js
@@ -1,15 +1,33 @@
 // Functions to increase and decrease the quantity of products in cart
-function increment(textBoxId){
+function increment(textBoxId) {
     var textBox = document.getElementById(textBoxId);
     var textBoxValue = Number(textBox.value);
     textBoxValue++;
     textBox.value = textBoxValue;
 }
-function decrement(textBoxId){
+function decrement(textBoxId) {
     var textBox = document.getElementById(textBoxId);
     var textBoxValue = Number(textBox.value);
-    if(textBoxValue != 1){
+    if (textBoxValue != 1) {
         textBoxValue--;
     }
     textBox.value = textBoxValue;
+}
+function showPrompt() {
+    bootbox.confirm({
+        message: "This is a confirm with custom button text and color! Do you like it?",
+        buttons: {
+            confirm: {
+                label: 'Yes',
+                className: 'btn-success'
+            },
+            cancel: {
+                label: 'No',
+                className: 'btn-danger'
+            }
+        },
+        callback: function (result) {
+            console.log('This was logged in the callback: ' + result);
+        }
+    });
 }
