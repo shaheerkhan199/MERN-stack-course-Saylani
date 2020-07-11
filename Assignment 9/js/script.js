59edd28eaa458897b68ec52a3a0a50f4792f24b0
M.Shaheer khan <m.shaheerkhan199@gmail.com>
2020-07-11 21:16:35 +0500

    Completed Todo App

--- Assignment 9/js/script.js
+++ Assignment 9/js/script.js
@@ -1,18 +1,50 @@
+// I have created the functionality of this Todo App using two different approaches
+// Approach or Method 1
+// function addTask() {
+//     var taskValue = document.getElementById("textBox").value;
+//     if(taskValue == ""){
+//         alert("Please Enter some task in textbox");
+//     }
+//     else{
+//         var orderList = document.getElementById("orderlist");
+//         newLI = document.createElement("li");
+//         newText = document.createTextNode(taskValue);
+//         newLI.appendChild(newText);
+//         orderList.appendChild(newLI);
+//         document.getElementById("textBox").value = "";
+//     }   
+// }
+// function deleteAllTask(){
+//     var orderList = document.getElementById("orderlist");
+//     orderList.innerHTML = "";   
+// }
+
+// Approach or Method 2
+var allTasks = ['Eat', 'Sleep', 'Code'];
+refreshList();
 function addTask() {
-    var taskValue = document.getElementById("textBox").value;
-    if(taskValue == ""){
+    var newTaskValue = document.getElementById("textBox").value;
+    if (newTaskValue == "") {
         alert("Please Enter some task in textbox");
     }
-    else{
-        var orderList = document.getElementById("orderlist");
-        newLI = document.createElement("li");
-        newText = document.createTextNode(taskValue);
-        newLI.appendChild(newText);
-        orderList.appendChild(newLI);
+    else {
+        allTasks.push(newTaskValue);
         document.getElementById("textBox").value = "";
-    }   
+        refreshList();
+    }
+}
+function refreshList() {
+    orderList = "";
+    orderList += "<ol>";
+    for (var i = 0; i < allTasks.length; i++) {
+        listItem = "<li>" + allTasks[i] + "</li>";
+        orderList += listItem;
+    }
+    orderList += "</ol>";
+    document.getElementById("orderListSpace").innerHTML = orderList;
 }
 function deleteAllTask(){
-    var orderList = document.getElementById("orderlist");
-    orderList.innerHTML = "";   
+    allTasks = [];
+    // document.getElementById("orderListSpace").innerHTML = "";  
+    refreshList(); 
 }
